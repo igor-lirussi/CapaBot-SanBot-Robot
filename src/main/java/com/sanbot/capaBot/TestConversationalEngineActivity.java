9cba122d3d964c3727921bedce2bd0373a96d643
unknown <igor.lirussi@studio.unibo.it>
2021-01-12 17:16:26 +0100

    fixed import conflicts gson

---
+++ src/main/java/com/sanbot/capaBot/TestConversationalEngineActivity.java
@@ -0,0 +1,13 @@
+package com.sanbot.capaBot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestConversationalEngineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_conversational_engine);
+    }
+}
